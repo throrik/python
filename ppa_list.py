5b0dac655d600cbf02875b75e1ea8e5923ee680c
Daniel Williamson <sporeot@outlook.com>
2023-05-19 00:37:08 +0100

    test commit

--- ppa_list.py
+++ ppa_list.py
@@ -4,6 +4,7 @@ import aptsources.sourceslist as sl
 
 sources = sl.SourcesList()
 
+# iterate over each source and find flag with disabled on upgrade
 for source in sources.list:
     if source.comment.lower().find("disabled on upgrade") >= 0:
         print(source)
