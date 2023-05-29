0d46afc78f2713cfd2dc6da0150c916a53e07ddf
Daniel Williamson <sporeot@outlook.com>
2023-05-29 18:16:36 +0100

    made the output more readable.

--- ppa_list.py
+++ ppa_list.py
@@ -1,10 +1,19 @@
-#! /user/bin/python3
+#! /usr/bin/python3
 
 import aptsources.sourceslist as sl
+from colorama import Fore, Style
+
+# Initialize colorama
+Fore.GREEN, Fore.YELLOW, Style.RESET_ALL
 
 sources = sl.SourcesList()
 
-# iterate over each source and find flag with disabled on upgrade
+# Iterate over each source and find flags with "disabled on upgrade"
 for source in sources.list:
-    if source.comment.lower().find("disabled on upgrade") >= 0:
-        print(source)
+    if "disabled on upgrade" in source.comment.lower():
+        # Display disabled PPAs in yellow
+        print(f"{Fore.YELLOW}{source}{Style.RESET_ALL}")
+    else:
+        # Display enabled PPAs in green
+        print(f"{Fore.GREEN}{source}{Style.RESET_ALL}")
+
